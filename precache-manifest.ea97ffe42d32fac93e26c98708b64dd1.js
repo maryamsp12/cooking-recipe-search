e2596d7fe398168be31322d1a7fc25411bb6a562
maryamsp12 <maryamsp12@example.com>
2019-02-04 07:13:06 +0200

    Updates

---
+++ precache-manifest.ea97ffe42d32fac93e26c98708b64dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8929a8fb25dfd93bdef",
+    "url": "/cooking-recipe-search/static/js/runtime~main.d8929a8f.js"
+  },
+  {
+    "revision": "635c17e5f785ea89c25f",
+    "url": "/cooking-recipe-search/static/js/main.635c17e5.chunk.js"
+  },
+  {
+    "revision": "0389060f4a610e03ffeb",
+    "url": "/cooking-recipe-search/static/js/1.0389060f.chunk.js"
+  },
+  {
+    "revision": "635c17e5f785ea89c25f",
+    "url": "/cooking-recipe-search/static/css/main.58339a54.chunk.css"
+  },
+  {
+    "revision": "0389060f4a610e03ffeb",
+    "url": "/cooking-recipe-search/static/css/1.95d8acb4.chunk.css"
+  },
+  {
+    "revision": "2b4021bee01c9f92ce5cad29eb7df177",
+    "url": "/cooking-recipe-search/index.html"
+  }
+];
